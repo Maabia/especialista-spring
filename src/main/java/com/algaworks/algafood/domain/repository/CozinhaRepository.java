a696c48dd57cf312cad0be86568d7f6cc8111ad9
Maabia <maabia.ferry16@gmail.com>
2021-11-22 11:48:07 -0300

    testeController implementado

--- src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
@@ -2,12 +2,16 @@ package com.algaworks.algafood.domain.repository;
 
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import com.algaworks.algafood.domain.model.Cozinha;
 
-
+@Repository
 public interface CozinhaRepository {
 	
 	List<Cozinha> listar();
+	List<Cozinha> consultarPorNome(String nome);
+	
 	Cozinha buscar(Long id);
 	Cozinha salvar(Cozinha cozinha);
 	void remover(Long id);
